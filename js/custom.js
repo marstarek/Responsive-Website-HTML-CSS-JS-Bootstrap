bda60032b1406e7494d72b8bafecb09bfad1d0dd
marstarek <tarekahmed1568@gmail.com>
2022-01-07 04:46:05 +0200

    jq slider

---
+++ js/custom.js
@@ -0,0 +1,49 @@
+$(document).ready(function() {
+    "use strict";
+    $(window).scroll(function() {
+        "use strict";
+        if ($(window).scrollTop() < 80) {
+            $(".navbar").css({
+                "margin-top": "-100px",
+                opacity: "0",
+            });
+            $(".navbar-light").css({
+                "background-color": "#ffc900ba",
+            });
+        } else {
+            $(".navbar").css({
+                "margin-top": "0px",
+                opacity: "1",
+            });
+            //   $(".navbar-brand img").css({
+            //       height: "35px",
+            //       "padding-top": "0px",
+            //   });
+            //   $(".navbar-nav>li>a").css({
+            //       "padding-top": "15px",
+            //   });
+        }
+    });
+});
+
+/* active menu item on click */
+$(document).ready(function() {
+    "use strict";
+
+    $(".navbar-nav li a").click(function() {
+        "use strict";
+
+        $(".navbar-nav li a").removeClass("active");
+
+        $(this).addClass("active");
+    });
+});
+$(document).ready(function() {
+    $(".slider").bxSlider({
+        slideWidth: 292.5,
+        auto: true,
+        minSlides: 1,
+        maxSlides: 3,
+        slideMargin: 50,
+    });
+});
